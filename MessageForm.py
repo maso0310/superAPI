b5e558b818c376ab6fa3e31b63bcd3996b19798f
maso0310 <balimon@hotmail.com>
2018-10-25 12:51:25 +0800

    'ok'

--- MessageForm.py
+++ MessageForm.py
@@ -94,13 +94,13 @@ def buttons_message(
             title=title,
             text=text,
             actions=[
-                DatetimepickerTemplateAction(
+                DatetimePickerTemplateAction(
                     label=label1,
                     data=data1,
                     mode='datetime',
-                    initial='2018-10-25t00:00'
-                    max='2018-12-31t23:59'
-                    min='2018-01-01t00:00'
+                    initial='2018-10-25'
+                    max='2018-12-31'
+                    min='2018-01-01'
                 ),
                 MessageTemplateAction(
                     label=label2,
--- app.py
+++ app.py
@@ -132,11 +132,12 @@ def handle_text_message(event):
 def handle_location_message(event):
     check_list = buttons_message(
     'https://pic.pimg.tw/k87110/1411026253-2090326290.jpg','你想做什麼','選擇以下功能',
-    '搜尋附近地點','偷偷回傳的訊息',
+    '搜尋附近地點','action=buy&itemid=1,
     '紀錄位置','紀錄位置',
     '沒做什麼','https://pic.pimg.tw/k87110/1411026253-2090326290.jpg'
     )
     print(check_list)
+
     line_bot_api.reply_message(event.reply_token,check_list)
 
 '''
