95140e7b6367a2e2693bc4fb1bd2d160adebf1a7
maso0310 <balimon@hotmail.com>
2018-10-27 00:53:04 +0800

    'ok'

--- Ten_Carousel.py
+++ Ten_Carousel.py
@@ -19,8 +19,6 @@ def Ten_Carousel_Template(
     message = TemplateSendMessage(
         alt_text='請查看手機的訊息',
         template=CarouselTemplate(
-            imageAspectRatio='square',
-            imageSize='cover',
             columns=[
                 CarouselColumn(
                     thumbnail_image_url=img_url1_1,
@@ -171,7 +169,9 @@ def Ten_Carousel_Template(
                             uri=url10_4
                         )
                     ]
-                )
+                ),
+            imageAspectRatio='square',
+            imageSize='cover',
             ]
         )
     )
