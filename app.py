1880a6d9f81faa074f5fac5dd0a3e7d8068ef025
maso0310 <balimon@hotmail.com>
2018-11-03 01:20:12 +0800

    'ok'

--- app.py
+++ app.py
@@ -113,8 +113,7 @@ def handle_text_message(event):
             reader = csv.reader(f)
             for row in reader:
                 print(row)
-                a.append(row)
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=a[0]))
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text=row[:]))
 
     elif "威淨SNAP酵素清潔劑，開團！" in msg:
         #商品縮圖網址
