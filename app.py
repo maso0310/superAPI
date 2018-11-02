9e9e378746a39998ead5bcd972143f4a179a4bac
maso0310 <balimon@hotmail.com>
2018-11-03 02:16:37 +0800

    'ok'

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def handle_text_message(event):
 #        with open('財務紀錄.csv',mode='r',encoding='utf-8') as f:
 #            for row in csv.DictReader(f):
 #                a = row['日期']+row['項目']+row['金額']
-        f = open('財務紀錄.txt','r', encoding='UTF-8')
+        f = open('財務紀錄.txt','rb')
         look = f.read()
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=look))
 
