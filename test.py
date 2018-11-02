39c8319ae50a1530952536e3e37d7ab55be7c473
maso0310 <balimon@hotmail.com>
2018-11-03 00:56:02 +0800

    'ok'

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-import time
-import re
+import csv
 
-text = "支出項目  吉普賽民歌餐廳240"
-item = '項目.*'
-money = '\d' 
-pay_for = re.findall(item,text)
-pay_money = re.findall(money,text)
-pay_for_text = pay_for[0]
-pay_money_text = pay_money[0]
-date = time.strftime('%Y-%m-%d',time.localtime())
-
-a = "已記錄"+date+"花費"+pay_for_text+pay_money_text+'元'
-print(a)
+with open('財務紀錄.csv',mode='r') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
