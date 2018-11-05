8cb1fe3cb7742ca17f914fb02b914b496853200c
maso0310 <balimon@hotmail.com>
2018-11-05 23:31:14 +0800

    'ok'

--- Sheetsu.py
+++
@@ -1,8 +0,0 @@
-#LineBot Sheet API https://sheetsu.com/apis/v1.0su/7e0b40d2e3f3
-
-# Read whole spreadsheet
-from sheetsu import SheetsuClient
-
-client = SheetsuClient("https://sheetsu.com/apis/v1.0su/7e0b40d2e3f3")
-print(client.read())
-client.create_one(time="測試",message="加入字串")
