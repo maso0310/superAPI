ef54526e5cd030bf7977bc512daa30b927a262ff
maso0310 <balimon@hotmail.com>
2018-11-10 23:57:23 +0800

    'ok'

--- app.py
+++ app.py
@@ -495,7 +495,7 @@ def handle_text_message(event):
             ]
             }
         }
-        j = new_from_json_dict('dict',s)
+        j = get_or_new_from_json_dict(s)
         line_bot_api.reply_message(event.reply_token,s)
 
 
