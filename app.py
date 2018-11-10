fd565f11f19f52990295d2e7c65cdc236706a122
maso0310 <balimon@hotmail.com>
2018-11-10 22:15:04 +0800

    'ok'

--- app.py
+++ app.py
@@ -379,7 +379,8 @@ def handle_text_message(event):
         line_bot_api.reply_message(event.reply_token,product)
 
     elif "最新公告" in msg:
-        shopbuddy = {
+        shopbuddy = 
+        {
         "type": "bubble",
         "header": {
             "type": "box",
