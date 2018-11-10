b443e91c3117cc83faf21a21e5a4ba573dc41a74
maso0310 <balimon@hotmail.com>
2018-11-11 00:37:16 +0800

    'ok'

--- app.py
+++ app.py
@@ -495,7 +495,7 @@ def handle_text_message(event):
             ]
             }
         }
-        j = FlexSendMessage(altText='最新購物工具！',contents=s)
+        j = FlexSendMessage(altText='最新購物工具！')
         line_bot_api.reply_message(event.reply_token,j)
 
 
